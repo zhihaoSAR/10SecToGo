481d536643c3a98c425206c64388757fa1fd9094
did <diegorospagan@gmail.com>
2020-04-04 19:36:39 +0200

    inicio pause menun

---
+++ Assets/Scripts/IU/pauseMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenuController : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused) resume();
+            else pause();
+        }
+        
+    }
+
+    public void pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+    public void resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+}
