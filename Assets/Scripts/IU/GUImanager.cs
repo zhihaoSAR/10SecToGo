983e1cc7f6b8681b062f0ce96862e440e8386b76
did <diegorospagan@gmail.com>
2020-04-16 16:27:21 +0200

    Inicio PowerUPS GUI
    imagen creacion....

--- Assets/Scripts/IU/GUImanager.cs
+++ Assets/Scripts/IU/GUImanager.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 public class GUImanager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject cronometro,ronda, sceneM;
+    public GameObject cronometro,ronda, sceneM,PowerUpsZone,powerObject;
+    private List<powers> powersUps = new List<powers>();
     private SceneController SceneManager;
-    private TMPro.TextMeshProUGUI c, r;
-    string Nronda="";
+    private TMPro.TextMeshProUGUI c, r,v;
+    string Nronda="",vida="1";
+    struct powers
+    {
+        public int time,totalTime;
+        public Effect id;
+        public GameObject PU;
+    } 
     void Start()
     {
         c = cronometro.GetComponent<TMPro.TextMeshProUGUI>();
@@ -29,4 +36,27 @@ public class GUImanager : MonoBehaviour
     {
         Nronda= "Ronda " + numRonda.ToString();   
     }
+    public void UpdateVida(int num)
+    {
+        vida = num.ToString();
+    }
+    public void createPowerUp(int time,Effect name)
+    {
+        bool esta = false;
+   
+        powersUps.ForEach(x=> {
+            if (x.id.Equals(name)){ esta = true;x.time = time; } //actualizar tiempo
+                
+                });
+        if (!esta)//crear objeto
+        {
+            powers p; p.time = time; p.totalTime = time; p.id = name; p.PU = Instantiate(powerObject,PowerUpsZone.transform)as GameObject;
+           // p.PU.transform.SetParent(PowerUpsZone.transform);
+           // p.PU.transform.localScale.Set(1f, 1f, 1f);
+
+            powersUps.Add(p);
+        }
+        
+    }
+
 }
